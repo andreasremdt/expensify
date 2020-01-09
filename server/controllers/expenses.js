2084f6683008462be1891798c288b18bfc65efbd
Andreas Remdt <me@andreasremdt.com>
2020-01-09 20:30:26 +0100

    Implement expenses controller

--- server/controllers/expenses.js
+++ server/controllers/expenses.js
@@ -1,15 +1,53 @@
+var db = require("../lib/database");
+
 module.exports = {
   index(response) {
-    response.send({ text: "Hello world" });
+    db.expenses.find({}, function(err, results) {
+      if (err) {
+        return response.status(500).send(err);
+      }
+
+      response.send(results);
+    });
   },
 
   show(response, request) {
-    response.send({ text: "single" });
+    db.expenses.findOne({ _id: request.id }, function(err, results) {
+      if (err) {
+        return response.status(500).send(err);
+      }
+
+      response.send(results || []);
+    });
   },
 
-  create(response) {},
+  create(response, request) {
+    db.expenses.insert(request.body, function(err, results) {
+      if (err) {
+        return response.status(500).send(err);
+      }
 
-  update(response) {},
+      response.send(results);
+    });
+  },
 
-  destroy(response) {}
+  update(response, request) {
+    db.expenses.update({ _id: request.id }, request.body, {}, function(err) {
+      if (err) {
+        return response.status(500).send(err);
+      }
+
+      response.send({});
+    });
+  },
+
+  destroy(response, request) {
+    db.expenses.remove({ _id: request.id }, {}, function(err) {
+      if (err) {
+        return response.status(500).send(err);
+      }
+
+      response.send({});
+    });
+  }
 };
